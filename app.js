d816dd6021197d671cf3e0ef441dfc7065451989
Jayden P <65550275+CollegeBoyBlue@users.noreply.github.com>
2021-05-04 09:23:02 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash')
+const path = require('path');
+const ExpressError = require('./utilities/ExpressError');
+const methodOverride = require('method-override');
+
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+
+mongoose.connect('mongodb://localhost:27017/MountainAir', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Online");
+});
+
+const app = express();
+
+app.engine('ejs', mate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'BadSecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.body.flash('success');
+    res.locals.error = req.body.flash('error');
+    next();
+})
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+
+app.get ('/', (req, res) => {
+    res.render('home')
+}) 
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Identifiable', 404))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'System Failure; Session Expired'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(3000, () => {
+    console.log('Local Server Connection: Verified')
+})
