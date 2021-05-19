237ae7feb6f166964292c31ec19f3e4bfd90e05b
Jayden P <65550275+CollegeBoyBlue@users.noreply.github.com>
2021-05-18 22:17:29 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 const mate = require('ejs-mate');
@@ -6,13 +10,23 @@ const flash = require('connect-flash')
 const path = require('path');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+
+const MongoDBStore = require('connect-mongo');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
+const databaseUrl = process.env.DB_URL || 'mongodb://localhost:27017/MountainAir'
 
-mongoose.connect('mongodb://localhost:27017/MountainAir', {
+mongoose.connect(databaseUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -33,33 +47,111 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+
+const secret = process.env.SECRET || 'Unknown'
+
+const store = MongoDBStore.create({
+    mongoUrl: databaseUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret,
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
-    secret: 'BadSecret',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+    "https://cdn.jsdelivr.net/npm/",
+
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dwjd5dnhj/",
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.success = req.body.flash('success');
-    res.locals.error = req.body.flash('error');
+    res.locals.currentUser = req.user;
+    console.log(req.currentUser, req.user);
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    
     next();
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get ('/', (req, res) => {
     res.render('home')
-}) 
+});
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Identifiable', 404))
@@ -71,6 +163,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
-    console.log('Local Server Connection: Verified')
+    console.log(`Local Server Connection: ${port}`)
 })
--- seeds/index.js
+++ seeds/index.js
@@ -19,15 +19,48 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 450; i++){
         const random1000 = Math.floor(Math.random() * 1000);
-        const price =Math.floor(Math.random() * 20) + 10;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6094b2268bf7fa061486d2f5',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque esse sapiente nam alias incidunt, vitae modi adipisci dicta necessitatibus id, quo praesentium molestias reprehenderit veritatis quasi aliquid maxime accusamus tenetur non facere blanditiis ad. Iste odio delectus ducimus, suscipit distinctio necessitatibus labore esse ad, expedita ex eaque corrupti quod voluptates.',
-            price 
+            price,
+            geometry: {
+                type: 'Point', 
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742762/MountainAir/p1j0087mwfaxxhpca9x5.jpg',
+                    filename: 'MountainAir/p1j0087mwfaxxhpca9x5'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742763/MountainAir/jfxkntj2sa8dfef6uy1y.jpg',
+                    filename: 'MountainAir/jfxkntj2sa8dfef6uy1y'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742764/MountainAir/ljuxxupj3kpa0okz1r0d.jpg',
+                    filename: 'MountainAir/ljuxxupj3kpa0okz1r0d'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742765/MountainAir/hvppmhwtt8tmxwcj6elw.jpg',
+                    filename: 'MountainAir/hvppmhwtt8tmxwcj6elw'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742767/MountainAir/kfnjhpjom3yuprnrgmcc.jpg',
+                    filename: 'MountainAir/kfnjhpjom3yuprnrgmcc'
+                },
+                {
+                   url: 'https://res.cloudinary.com/dwjd5dnhj/image/upload/v1620742769/MountainAir/dg93hstsjdyzz0nrinj3.jpg',
+                   filename: 'MountainAir/dg93hstsjdyzz0nrinj3'
+                }
+            ] 
         })
         await camp.save();
     }
